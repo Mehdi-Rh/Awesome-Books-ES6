8c8df468feaae2723bdfaa1f7b4b12998674ff80
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-02-28 10:17:28 +0100

    Fix reload problem

--- index.js
+++ index.js
@@ -9,29 +9,36 @@ const addActive = document.querySelectorAll('.section');
 theForm.addEventListener('submit', () => {
   const newBook = new Book(bookTitle.value, bookAuthor.value);
   newBook.addBook();
+
   Book.displayUI();
+  location.reload();
 
 });
 
 Book.displayUI();
 
 const removeButton = document.querySelectorAll('.remove-btn');
-// removeButton.forEach((item) => item.addEventListener('click', () => {
-//   const deleteBook = new Book(bookTitle.value, bookAuthor.value, this.id);
-//   deleteBook.removeBook();
-// }));
+removeButton.forEach((item,i) => item.addEventListener('click', () => {
+  const deleteBook = new Book(bookTitle.value, bookAuthor.value, i);
+  deleteBook.removeBook();
+}));
 
-for (let index = 0; index < removeButton.length; index++) {
-  const element = removeButton[index];
-  element.addEventListener('click', () => {
-      const deleteBook = new Book(bookTitle.value, bookAuthor.value, index);
-      deleteBook.removeBook();
-    })
-  
-}
+// for (let index = 0; index < removeButton.length; index++) {
+//   const element = removeButton[index];
+//   element.addEventListener('click', () => {
+//       const deleteBook = new Book(bookTitle.value, bookAuthor.value, index);
+//       deleteBook.removeBook();
+//       location.reload();
+
+//     })
+// }
 
 listBtn.forEach((btn, i) => {
+
   btn.onclick = () => {
+    if (i===0) {
+      window.location.reload();
+    }
     listBtn.forEach((oldBtn) => {
       oldBtn.classList.remove('active');
     });
